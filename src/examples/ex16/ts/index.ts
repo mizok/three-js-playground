492de48575b437cffc9d907d9cd852a122ffbf17
mizok <karasunohina@gmail.com>
2022-08-03 00:55:00 +0800

    update

--- src/examples/ex16/ts/index.ts
+++ src/examples/ex16/ts/index.ts
@@ -1,8 +1,10 @@
-import { AmbientLight, AxesHelper, BoxGeometry, Clock, ConeGeometry, DirectionalLight, Mesh, MeshStandardMaterial, PerspectiveCamera, Scene, TorusGeometry, TorusKnotGeometry, Vector2, Vector3, WebGLRenderer } from 'three';
+import { AmbientLight, AxesHelper, BoxGeometry, Clock, ConeGeometry, DirectionalLight, Group, Mesh, MeshStandardMaterial, PerspectiveCamera, Scene, TorusGeometry, TorusKnotGeometry, Vector2, Vector3, WebGLRenderer } from 'three';
 import { debounce } from 'lodash';
+import { gsap, Power2 } from 'gsap'
 
 function main() {
-  const { scene, renderer, camera, clock, axis } = new RenderEnv();
+  let env: RenderEnv;
+  const { scene, renderer, camera, clock, axis } = env = new RenderEnv();
   const torusGeo = new TorusGeometry(1, 0.5, 50, 50);
   const coneGeo = new ConeGeometry(1, 2, 30, 30);
   const torusKnotGeo = new TorusKnotGeometry(1, 0.5, 50, 50);
@@ -13,16 +15,65 @@ function main() {
   const coneMesh = new Mesh(coneGeo, mat.clone());
   const torusKnotMesh = new Mesh(torusKnotGeo, mat.clone());
   const gap = 10;
-  coneMesh.position.set(0, -1 * gap, 0);
-  torusKnotMesh.position.set(0, -2 * gap, 0);
   const meshs = [torusMesh, coneMesh, torusKnotMesh]
-  scene.add(...meshs);
+  const meshGroups = meshs.map((o, i) => {
+    const group = new Group();
+    group.add(o);
+    group.position.set(0, -i * gap, 0)
+    return group;
+  })
+
+  const cameraGroup = new Group();
+  cameraGroup.add(camera);
+  scene.add(cameraGroup, ...meshGroups);
+
+  let prevSection = 0;
 
   document.body.addEventListener('scroll', () => {
-    const progress = document.body.scrollTop / (document.body.scrollHeight - document.body.getBoundingClientRect().height);
-    const y = -progress * gap * (meshs.length - 1);
+    const height = document.body.getBoundingClientRect().height
+    const allHeight = document.body.scrollHeight - height;
+    const progress = document.body.scrollTop / allHeight;
+    const cellHeight = gap * (meshs.length - 1)
+    const y = -progress * cellHeight;
+    let section = document.body.scrollTop / (allHeight / (meshs.length - 1));
+    section = Math.round(section);
+    let currentSection = section;
     camera.position.setY(y);
+    if (currentSection !== prevSection) {
+      gsap.to(meshGroups[currentSection].rotation, {
+        duration: 1,
+        ease: Power2.easeOut,
+        y: '+=9'
+      })
+      prevSection = section;
+    }
+
   })
+  const speed = 5;
+  document.body.addEventListener('mousemove', (ev) => {
+    const rect = renderer.domElement.getBoundingClientRect();
+    env.cursor.x = ((ev.clientX - rect.left) / rect.width - 0.5) * 2;
+    env.cursor.y = -((ev.clientY - rect.top) / rect.height - 0.5) * 2;
+
+  })
+
+  let prevTime = 0;
+
+  env.frameListener = (time) => {
+    torusMesh.rotation.x = time * 0.1;
+    torusMesh.rotation.y = time * 0.3;
+    coneMesh.rotation.x = time * 0.1
+    coneMesh.rotation.y = time * 0.4
+    torusKnotMesh.rotation.x = time * 0.3
+    torusKnotMesh.rotation.y = time * 0.5;
+    const gapTime = time - prevTime;
+    prevTime = time;
+    cameraGroup.position.set(
+      cameraGroup.position.x + (env.cursor.x - cameraGroup.position.x) * speed * gapTime,
+      cameraGroup.position.y + (env.cursor.y - cameraGroup.position.y) * speed * gapTime,
+      0
+    )
+  }
 }
 
 
@@ -32,7 +83,8 @@ class RenderEnv {
   camera: PerspectiveCamera;
   renderer: WebGLRenderer;
   clock: Clock;
-  cursor: Vector2;
+  cursor: Vector2 = new Vector2();
+  frameListener: (time: number) => void = (time) => { }
   constructor() {
     this.init();
   }
@@ -73,6 +125,7 @@ class RenderEnv {
 
 
     const tick = () => {
+      this.frameListener(clock.getElapsedTime())
       renderer.render(scene, camera);
       requestAnimationFrame(tick);
     }
